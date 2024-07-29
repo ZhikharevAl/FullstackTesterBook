f973abb643a17846c9b6adadc0ca677bb58ce678
AlexeyZh <waltafunk@gmail.com>
2024-07-29 23:24:52 +0300

    chore: add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    // JUnit 5
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.3")
+
+    // Selenium
+    implementation("org.seleniumhq.selenium:selenium-java:4.23.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
